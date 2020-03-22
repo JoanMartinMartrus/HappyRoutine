456bd259edf0e139edc57c3cdc813b48a51c5f23
JoanMartinMartrus <joan71995@gmail.com>
2020-03-22 18:33:31 +0100

    JMM 20/03/2020 - LY05 Settings layout

--- app/src/main/java/com/example/happyroutine/settings.kt
+++ app/src/main/java/com/example/happyroutine/settings.kt
@@ -1,7 +1,9 @@
 package com.example.happyroutine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class settings : AppCompatActivity() {
 
@@ -9,4 +11,10 @@ class settings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
     }
+
+    fun goToUserInformation(view: View) {
+        val intent = Intent(this, user_information::class.java)
+        startActivity(intent)
+    }
+
 }
