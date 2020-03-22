f60eb06d82358d7b9fa784889a3e99965072b4a9
cristiansoftware <jrodrisu34@alumnes.ub.edu>
2020-03-22 22:07:45 +0100

    LY08v2

--- app/src/main/java/com/example/happyroutine/exercise.kt
+++ app/src/main/java/com/example/happyroutine/exercise.kt
@@ -4,20 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class routine : AppCompatActivity() {
+class exercise : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_routine)
-    }
-
-    fun goToExercise(view: View) {
-        val intent = Intent(this, exercise::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_main)
     }
 
     fun goToSettings(view: View) {
         val intent = Intent(this, settings::class.java)
         startActivity(intent)
     }
-
 }
