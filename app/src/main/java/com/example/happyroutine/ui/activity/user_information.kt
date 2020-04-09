2a5a8b6611e63d5297aa1cb135328e78bd965541
JoanMartinMartrus <joan71995@gmail.com>
2020-04-09 12:35:32 +0200

    JMM 02/04/20 - FO4 UserInformation - Set errors when some fields is empty and hide/unhide platform questionary when platform checkbox is checked/unchecked

--- app/src/main/java/com/example/happyroutine/ui/activity/user_information.kt
+++ app/src/main/java/com/example/happyroutine/ui/activity/user_information.kt
@@ -18,7 +18,28 @@ class user_information : AppCompatActivity() {
         edit_text_birthday.setOnClickListener {
             showDatePickerDialog()
         }
+
+        checkBoxPlatformAnswer.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                linearlayout_platform.visibility=View.VISIBLE
+            }else {
+                linearlayout_platform.visibility=View.INVISIBLE
+            }
+        }
     }
+
+    fun goToUserExercise(view: View) {
+        clearErrors()
+        if (checkFields()) {
+            saveData()
+        }
+
+    }
+
+    private fun saveData() {
+        
+    }
+
 
     private fun showDatePickerDialog() {
         val newFragment = DatePickerFragment.newInstance(DatePickerDialog.OnDateSetListener { _, year, month, day ->
@@ -30,9 +51,86 @@ class user_information : AppCompatActivity() {
         newFragment.show(supportFragmentManager, "datePicker")
     }
 
-    fun goToUserExercise(view: View) {
-        val intent = Intent(this, routine::class.java)
-        startActivity(intent)
+
+
+    private fun checkFields(): Boolean{
+
+        if (edit_text_name.text.toString().isEmpty()) {
+            edit_text_name.error = "Please enter the name"
+            edit_text_name.requestFocus()
+            return false
+        }
+
+        if (edit_text_surname.text.toString().isEmpty()) {
+            edit_text_surname.error = "Please enter the surname"
+            edit_text_surname.requestFocus()
+            return false
+        }
+
+        if (radiogroup_gender.checkedRadioButtonId == -1) {
+            tv_gender.setError("Please choose gender")
+            tv_gender.requestFocus()
+            return false
+        }
+
+        if (edit_text_birthday.text.toString().isEmpty()) {
+            edit_text_birthday.error = "Please enter your birthday"
+            edit_text_birthday.requestFocus()
+            return false
+        }
+
+        if (edit_text_location.text.toString().isEmpty()) {
+            edit_text_location.error = "Please enter your city"
+            edit_text_location.requestFocus()
+            return false
+        }
+
+        if (edit_text_height.text.toString().isEmpty()) {
+            edit_text_height.error = "Please enter your height"
+            edit_text_height.requestFocus()
+            return false
+        }
+
+        if (edit_text_weight.text.toString().isEmpty()) {
+            edit_text_weight.error = "Please enter your weight"
+            edit_text_weight.requestFocus()
+            return false
+        }
+
+        if ((rg_line1.checkedRadioButtonId==-1) and (rg_line2.checkedRadioButtonId==-1)) {
+            tv_objectives.setError("Please choose objective")
+            tv_objectives.requestFocus()
+            return false
+        }
+
+        if ( (!offerExerciseAdviceCheckbox.isChecked and !offerDietAdviceCheckbox.isChecked and
+                    !offerCompanyCheckbox.isChecked) and checkBoxPlatformAnswer.isChecked ) {
+            tv_offer.setError("Select at least one item from offer")
+            tv_offer.requestFocus()
+            return false
+        }
+
+        if ( (!needsExerciseAdviceCheckbox.isChecked and !needsDietAdviceCheckbox.isChecked and
+                    !needsCompanyCheckbox.isChecked) and checkBoxPlatformAnswer.isChecked ) {
+            tv_needs.setError("Select at least one item from needs")
+            tv_needs.requestFocus()
+            return false
+        }
+
+        return true
+    }
+
+    private fun clearErrors() {
+        edit_text_name.setError(null)
+        edit_text_surname.setError(null)
+        tv_gender.setError(null)
+        edit_text_birthday.setError(null)
+        edit_text_location.setError(null)
+        edit_text_weight.setError(null)
+        edit_text_height.setError(null)
+        tv_objectives.setError(null)
+        tv_offer.setError(null)
+        tv_needs.setError(null)
     }
 
 }
