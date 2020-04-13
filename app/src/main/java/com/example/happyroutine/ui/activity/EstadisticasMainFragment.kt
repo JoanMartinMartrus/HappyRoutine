a3d0536071e215c712b159e3d1e2430b028230b7
Pol Costa <polct1999@gmail.com>
2020-04-13 14:02:20 +0200

    Weight statistics almost ready

--- app/src/main/java/com/example/happyroutine/ui/activity/EstadisticasMainFragment.kt
+++ app/src/main/java/com/example/happyroutine/ui/activity/EstadisticasMainFragment.kt
@@ -1,11 +1,13 @@
 package com.example.happyroutine.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.happyroutine.R
+import kotlinx.android.synthetic.main.fragment_estadisticas_main.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,4 +22,13 @@ class EstadisticasMainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_estadisticas_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_weight.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, Estadisticas_weight::class.java)
+                it.startActivity(intent)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/happyroutine/ui/activity/Estadisticas_weight.kt
+++ app/src/main/java/com/example/happyroutine/ui/activity/Estadisticas_weight.kt
@@ -1,39 +1,42 @@
 package com.example.happyroutine.ui.activity
 
+import android.graphics.Color
 import android.os.Bundle
-
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.happyroutine.R
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import kotlinx.android.synthetic.main.activity_estadisticas_weight.*
+
 
 class Estadisticas_weight : AppCompatActivity() {
 
-    private val firebaseAuth: FirebaseAuth? = null
-    private val firestore: FirebaseFirestore? = null
-    private val weightEntries = ArrayList<Float>()
-    //private val dateEntries = ArrayList<Timestamp>()
-    //val users = db.collection("users")
-    //if(currentUser.matches()){
-    // val weightEntries = users.collection("weightTrack").document("weight")
-    //weightEntries.set(NEW_WEIGHT)
+    //lateinit var auth: FirebaseAuth
+    private var weightEntries = ArrayList<Float>()
+    private var dateEntries = ArrayList<Timestamp>()
 
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_estadisticas_weight)
+        //val userUid = FirebaseAuth.getInstance().currentUser?.uid
 
         weightEntries.add(75f)
-        //weightEntries.add(75.5f)
-        //weightEntries.add(76f)
+        weightEntries.add(75.5f)
+        weightEntries.add(76f)
 
         drawChart()
     }
@@ -44,18 +47,67 @@ class Estadisticas_weight : AppCompatActivity() {
             weightEntry.requestFocus()
             return
         }
-        //val currentDate = Timestamp.now()
-        //val newPosition = entries.size.toFloat() + 1
+        //Get new weight & date entries and update the database
+        val db = FirebaseFirestore.getInstance().collection("users")
+        db.document(FirebaseAuth.getInstance().currentUser!!.uid).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val document = task.result
+                    if (document!!.exists()) {
+                        weightEntries = document["weight"] as ArrayList<Float>
+                        dateEntries = document["date"] as ArrayList<Timestamp>
+                    }
+                }
+            }
         val newWeight = weightEntry.text.toString().toFloat()
-        //entries.add(Entry(newPosition, newWeight))
+        val newDate = Timestamp.now()
         weightEntries.add(newWeight)
+        dateEntries.add(newDate)
+
+        db.document(FirebaseAuth.getInstance().currentUser!!.uid)
+            .set(weightEntries, SetOptions.merge())
+            .addOnSuccessListener {
+                Log.i("DB", "Data stored succesfully")
+            }
+            .addOnFailureListener {e ->
+                Log.i("DB", "Something went wrong",e)
+                Toast.makeText(baseContext, "Something went wrong$e",
+                    Toast.LENGTH_LONG).show()
+            }
+
+        db.document(FirebaseAuth.getInstance().currentUser!!.uid)
+            .set(dateEntries, SetOptions.merge())
+            .addOnSuccessListener {
+                Log.i("DB", "Data stored succesfully")
+            }
+            .addOnFailureListener {e ->
+                Log.i("DB", "Something went wrong",e)
+                Toast.makeText(baseContext, "Something went wrong$e",
+                    Toast.LENGTH_LONG).show()
+            }
+
+        /*val weightTrack = hashMapOf(
+            "weightEntries" to weightEntries,
+            "dateEntries" to dateEntries
+        )*/
+        //Refresh chart
         showLimits()
         drawChart()
     }
 
     private fun drawChart(){
         //Get weight and date lists from Firebase
-
+        val db = FirebaseFirestore.getInstance().collection("users")
+        db.document(FirebaseAuth.getInstance().currentUser!!.uid).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val document = task.result
+                    if (document!!.exists()) {
+                        weightEntries = document["weight"] as ArrayList<Float>
+                        dateEntries = document["date"] as ArrayList<Timestamp>
+                    }
+                }
+            }
 
         val entries = ArrayList<Entry>()
         var index = 1f
@@ -109,24 +161,35 @@ class Estadisticas_weight : AppCompatActivity() {
     }
 
     private fun showLimits(){
-        val overweight_value = 80f
-        val overweight_limit = LimitLine(overweight_value, "Overweight")
-        overweight_limit.lineWidth = 4f
-        overweight_limit.enableDashedLine(10f,10f,0f)
-        overweight_limit.labelPosition = LimitLine.LimitLabelPosition.RIGHT_TOP
-        overweight_limit.textSize = 10f
-        //overweight_limit.lineColor = R.color.red
+        var currentHeight = 0f
+        val db = FirebaseFirestore.getInstance().collection("users")
+        db.document(FirebaseAuth.getInstance().currentUser!!.uid).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val document = task.result
+                    if (document!!.exists()) {
+                        currentHeight = document["height"] as Float
+                    }
+                }
+            }
+        val overweightValue = (currentHeight/100) * (currentHeight/100) * 25f
+        val overweightLimit = LimitLine(overweightValue, "Overweight")
+        overweightLimit.lineWidth = 4f
+        overweightLimit.enableDashedLine(10f,10f,0f)
+        overweightLimit.labelPosition = LimitLine.LimitLabelPosition.RIGHT_TOP
+        overweightLimit.textSize = 10f
+        overweightLimit.lineColor = Color.rgb(254,10,10)
 
-        val lowweight_value = 60f
-        val lowweight_limit = LimitLine(lowweight_value, "Low weight")
-        lowweight_limit.lineWidth = 4f
-        lowweight_limit.enableDashedLine(10f,10f,0f)
-        lowweight_limit.labelPosition = LimitLine.LimitLabelPosition.RIGHT_TOP
-        lowweight_limit.textSize = 10f
-        //lowweight_limit.lineColor = R.color.wallet_holo_blue_light
+        val lowweightValue = (currentHeight/100) * (currentHeight/100) * 18.4f
+        val lowweightLimit = LimitLine(lowweightValue, "Low weight")
+        lowweightLimit.lineWidth = 4f
+        lowweightLimit.enableDashedLine(10f,10f,0f)
+        lowweightLimit.labelPosition = LimitLine.LimitLabelPosition.RIGHT_TOP
+        lowweightLimit.textSize = 10f
+        lowweightLimit.lineColor = Color.rgb(10, 10, 254)
 
-        lineChart.axisLeft.addLimitLine(overweight_limit)
-        lineChart.axisLeft.addLimitLine(lowweight_limit)
+        lineChart.axisLeft.addLimitLine(overweightLimit)
+        lineChart.axisLeft.addLimitLine(lowweightLimit)
         return
     }
 }
