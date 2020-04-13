d9c1606f4a58358a081f24cb6d41408f0c560b34
JoanMartinMartrus <joan71995@gmail.com>
2020-04-13 11:55:16 +0200

    JMM 02/04/20 - FO4 UserInformation - Store data in firestore

--- app/src/main/java/com/example/happyroutine/ui/activity/user_information.kt
+++ app/src/main/java/com/example/happyroutine/ui/activity/user_information.kt
@@ -2,18 +2,29 @@ package com.example.happyroutine.ui.activity
 
 import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.happyroutine.R
 import com.example.happyroutine.ui.dialog.DatePickerFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_user_information.*
+import java.util.*
 
 class user_information : AppCompatActivity() {
+
+    lateinit var db : CollectionReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_information)
+
+        db = FirebaseFirestore.getInstance().collection("users")
 
         edit_text_birthday.setOnClickListener {
             showDatePickerDialog()
@@ -31,13 +42,110 @@ class user_information : AppCompatActivity() {
     fun goToUserExercise(view: View) {
         clearErrors()
         if (checkFields()) {
-            saveData()
+            storeUserInformationData()
         }
-
     }
 
-    private fun saveData() {
-        
+    private fun storeUserInformationData() {
+        val uid = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        val name = edit_text_name.text.toString()
+        val surname = edit_text_surname.text.toString()
+        val email = FirebaseAuth.getInstance().currentUser?.email
+        val gender = findViewById<RadioButton>(radiogroup_gender.checkedRadioButtonId).text.toString()
+        val day = edit_text_birthday.text.toString().split("/")[0].trim().toInt()
+        val month = edit_text_birthday.text.toString().split("/")[1].trim().toInt() - 1 // January is 0
+        val year = edit_text_birthday.text.toString().split("/")[2].trim().toInt()
+        val date = Date(year, month, day)
+        val location = edit_text_location.text.toString()
+        val height = edit_text_height.text.toString().toInt()
+        val weight = edit_text_weight.text.toString().toInt()
+        val objective = getObjective()
+        val diet = getDiet()
+        val participantPlatform = checkBoxPlatformAnswer.isChecked
+        val offer = getOffer()
+        val needs = getNeeds()
+
+
+        val data = hashMapOf(
+            "uid" to uid,
+            "name" to name,
+            "surname" to surname,
+            "email" to email,
+            "gender" to gender,
+            "birthday" to date,
+            "location" to location,
+            "height" to height,
+            "weight" to weight,
+            "objective" to objective,
+            "diet" to diet,
+            "participantPlatform" to participantPlatform,
+            "offer" to offer,
+            "needs" to needs
+        )
+
+       db.document(uid)
+           .set(data)
+           .addOnSuccessListener {
+               Log.i("DB", "Data stored succesfully")
+               startActivity(Intent(this, Navigation_bar_main::class.java))
+               finish()
+           }
+           .addOnFailureListener {e ->
+               Log.i("DB", "Something went wrong",e)
+               Toast.makeText(baseContext, "Something went wrong" + e.toString(),
+                   Toast.LENGTH_LONG).show()
+           }
+    }
+
+    private fun getOffer(): List<String> {
+        var offerList = mutableListOf<String>()
+        if (offerCompanyCheckbox.isChecked) {
+            offerList.add(offerCompanyCheckbox.text.toString())
+        }
+        if (offerDietAdviceCheckbox.isChecked) {
+            offerList.add(offerDietAdviceCheckbox.text.toString())
+        }
+        if (offerExerciseAdviceCheckbox.isChecked) {
+            offerList.add(offerExerciseAdviceCheckbox.text.toString())
+        }
+        return offerList
+    }
+
+
+    private fun getNeeds(): List<String> {
+        var needsList = mutableListOf<String>()
+        if (needsCompanyCheckbox.isChecked) {
+            needsList.add(needsCompanyCheckbox.text.toString())
+        }
+        if (needsDietAdviceCheckbox.isChecked) {
+            needsList.add(needsDietAdviceCheckbox.text.toString())
+        }
+        if (needsExerciseAdviceCheckbox.isChecked) {
+            needsList.add(needsExerciseAdviceCheckbox.text.toString())
+        }
+        return needsList
+    }
+
+    private fun getDiet(): List<String> {
+        var dietList  = mutableListOf<String>()
+        if (checkBoxLactoseIntolerant.isChecked) {
+            dietList.add(checkBoxLactoseIntolerant.text.toString())
+        }
+        if (checkBoxCeliac.isChecked) {
+            dietList.add(checkBoxCeliac.text.toString())
+        }
+        if (checkBoxVegetarian.isChecked) {
+            dietList.add(checkBoxVegetarian.text.toString())
+        }
+        return dietList
+    }
+
+    private fun getObjective(): String {
+        if (rg_line1.checkedRadioButtonId != -1 ) {
+            return findViewById<RadioButton>(rg_line1.checkedRadioButtonId).text.toString()
+        }
+
+        return findViewById<RadioButton>(rg_line2.checkedRadioButtonId).text.toString()
     }
 
 
